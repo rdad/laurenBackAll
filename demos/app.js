8c2c34057835606f44bcc94f60a18ef0c807f2d7
Thierry Tranchina <akindofceasar@gmail.com>
2015-06-28 20:08:08 +0200

    first commit: demo integration in place

---
+++ demos/app.js
@@ -0,0 +1,62 @@
+(function(ctx){
+
+	var $ui,$input,$triples, db, step,
+		triple = ["","",""],
+		triple_class = ["node", "predicat","subject"];
+
+	var app = {
+
+		start: function(config){
+
+			db 		= config.db;
+			step 	= 0;
+
+			$ui 	 = $('#ui'),
+			$triples = $('#triples')
+			$input 	 = $ui.children('input');
+
+			$('body').on('keypress', self.on_keypress);
+
+			$triples.css('height', (window.innerHeight-30)+'px');
+
+			$input.focus();
+		},
+
+		on_keypress: function(e){
+
+			if(e.which == 13) {
+
+		        triple[step] = $input.val();
+		        $input.val('').before('<div class="'+triple_class[step]+'">'+triple[step]+'</div>');
+		        step++;
+
+		        if(step == 1){
+		        	$input.addClass('predicat');
+		        }else{
+		        	$input.removeClass('predicat');
+		        }
+
+		        if(step == 3){
+		        	
+		        	$ui.children('div').remove();
+		        	$triples.append('<p>'+triple.join(' ')+'</p>');
+		        	triple = ["","",""];
+		        	step = 0;
+		        }
+		    }
+		}
+
+		
+	};
+
+	var self 		= app;
+	ctx.app 		= app;
+
+
+	// --- private methods ---
+	
+	function clog( str ){
+		console.log(str);
+	}
+
+})(window);
---
+++ src/lba.js
@@ -0,0 +1,35 @@
+(function(ctx){
+
+
+
+	var laurenbackall = {
+
+		VERSION: 0.1,
+
+		init: function(){
+
+		},
+
+		set_input_ui: function(){
+			
+		}
+		
+	};
+
+	var self 		= laurenbackall;
+	ctx.lba 		= laurenbackall;
+
+	init();
+
+	// --- private methods ---
+	
+	function clog( str ){
+		console.log(str);
+	}
+
+	function init(){
+			
+		
+	}
+
+})(window);
